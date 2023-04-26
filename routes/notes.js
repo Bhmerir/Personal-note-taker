9fce3aeea0a015256e466eb0f475c2851c27f952
Bahareh <mer_ir@yahoo.com>
2023-04-25 22:57:05 -0700

    add get route to read notes from file, and post route to add note, and show existing notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,25 @@
 const notes = require('express').Router();
-
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
 
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
-  console.info(`${req.method} request received for notes`);
+    readFromFile('./db/db.json').then((data) => {
+        res.json(JSON.parse(data))
+    })
+});
+
+notes.post('/', (req, res) => {
+    const {title, text} = req.body;
+    const newNote = {
+        title,
+        text,
+        id: uuid()
+    }
+    readAndAppend(newNote, './db/db.json');
+    readFromFile('./db/db.json').then((data) => {
+        res.json(JSON.parse(data))
+    })
 });
 
 module.exports = notes;
